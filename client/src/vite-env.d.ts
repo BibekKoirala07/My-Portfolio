b987432efd5e3cf34ef93e070987fd15b0cf07ba
Bibek Koirala <www.koiralabibek2058@gmail.com>
2024-10-24 23:56:10 +0545

    real

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -1,2 +1,23 @@
 /// <reference types="vite/client" />
 declare module "*.css";
+
+declare module "*.svg" {
+  const content: any;
+  export default content;
+}
+
+interface ImportMetaEnv {
+  readonly VITE_PROD_FRONTEND_URL: string;
+  readonly VITE_DEV_FRONTEND_URL: string;
+  readonly VITE_PROD_BACKEND_URL: string;
+  readonly VITE_DEV_BACKEND_URL: string;
+  readonly MODE: string;
+  readonly BASE_URL: string;
+  readonly DEV: boolean;
+  readonly PROD: boolean;
+  // Add other env variables here
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
