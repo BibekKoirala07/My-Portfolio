500c18b7ade413dd9c01eb73c6f0c7802d70de58
Bibek Koirala <www.koiralabibek2058@gmail.com>
2024-10-25 10:33:22 +0545

    real

--- client/src/env.d.ts
+++ client/src/env.d.ts
@@ -1,10 +1,10 @@
-// interface ImportMetaEnv {
-//   readonly VITE_PROD_BACKEND_URL: string;
-//   readonly VITE_DEV_BACKEND_URL: string;
-//   readonly NODE_ENV: "development" | "production";
-//   // Add other env variables here
-// }
+interface ImportMetaEnv {
+  readonly VITE_PROD_BACKEND_URL: string;
+  readonly VITE_DEV_BACKEND_URL: string;
+  readonly NODE_ENV: "development" | "production";
+  // Add other env variables here
+}
 
-// interface ImportMeta {
-//   readonly env: ImportMetaEnv;
-// }
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
